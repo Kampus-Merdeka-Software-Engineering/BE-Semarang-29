a1cdb6956abb7fe2ce8c7f3d8f1b0e9f453e8e99
Jian Jeraus Young <jianjerausyoung37@gmail.com>
2023-10-09 12:21:48 +0700

    changes

--- associations.js
+++
@@ -1,8 +0,0 @@
-// // associations.js
-// import Patient from './models/PatientModels.js';
-// import Room from './models/RoomModels.js';
-
-// Room.belongsTo(Patient);
-// Patient.hasOne(Room);
-
-// export { Patient, Room };
